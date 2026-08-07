bd1cfa5ee4b223d914d7baacc8e3d9264b838f16
kailash suthar <kailashsuthar4488@gmail.com>
2026-08-07 11:58:35 +0530

    Auth Api

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -7,7 +7,7 @@ const registerController = async (req, res) => {
 
         if(!req.body) {
             return res.status(400).json({
-                message : "Enter All Credentials"
+                message : "Enter All Details"
             })
         }
 
@@ -108,38 +108,90 @@ const registerController = async (req, res) => {
 
 const loginController = async (req, res) => {
     try {
-        if(!res.body) {
+        if(!req.body) {
             return res.status(400).json({
-                message : "Something went wrong",
-                error : error.message
+                message : "Enter All Details",
             });
         }
 
-        const {username, password} = res.body;
+        const {email, password} = req.body;
+
+        if(!email.trim() && !password.trim()) {
+            return res.status(400).json({
+                message : "Enter All Credentials"
+            })
+        }
         
-        if(!username.trim()) {
+        if(!email.trim()) {
             res.status(400).json({
                 message : "Username is required"        
             });
         }
 
-        if(!email.trim()) {
+        if(!password.trim()) {
             res.status(400).json({
                 message : "Email is required"
             });
         }
 
-        const isUserExists = await userModel.findOne({
+        const user = await userModel.findOne({
             email : email
         });
 
-        if(!isUserExists) {
-            return res.status(400).json({
+        if(!user) {
+            return res.status(404).json({
                 message : "User does not exists"
             });
         }
 
-        res.send(isUserExists);
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordCorrect) {
+            return res.status(409).json({
+                message : "Password is incorrect"
+            })
+        }
+
+        const access_token = JWT.sign({
+            id : user._id,
+            username : user.username,
+            email : user.email
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn : "3h"
+        }
+    );
+
+        const refresh_token = JWT.sign({
+            id : user._id,
+            username : user.username,
+            email : user.email
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn : "7d"
+        }
+    );
+
+    res.cookie("access_token", access_token, {
+        httpOnly : true,
+        secure : true,
+        samSite : "strict"
+    });
+
+    res.cookie("refresh_token", refresh_token, {
+        httpOnly : true,
+        secure : true,
+        sameSite : "strict"
+    });
+
+    res.status(201).json({
+        message : "User login successfully",
+        id : user._id,
+        username : user.username,
+        email : user.email
+    });
 
     } catch (error) {
         res.status(400).json({
@@ -151,7 +203,7 @@ const loginController = async (req, res) => {
 
 const getMeController = async (req, res) => {
     try {
-
+        
     } catch (error) {
         res.status(400).json({
             message : "Something went wrong",
